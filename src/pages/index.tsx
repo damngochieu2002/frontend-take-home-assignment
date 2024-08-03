25becda61a62650600f7be54dc3f0bb22365fd3e
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-08-03 16:15:39 +0700

    answer question 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@radix-ui/react-tabs'
+
 import { CreateTodoForm } from '@/client/components/CreateTodoForm'
 import { TodoList } from '@/client/components/TodoList'
+import { api } from '@/utils/client/api'
 
 /**
  * QUESTION 6:
@@ -17,6 +21,15 @@ import { TodoList } from '@/client/components/TodoList'
  */
 
 const Index = () => {
+  const [statusFilter, setStatusFilter] = useState('all')
+  const { data: todos = [] } = api.todo.getAll.useQuery({
+    statuses: ['completed', 'pending'],
+  })
+
+  const filteredTodos = todos.filter((todo) =>
+    statusFilter === 'all' ? true : todo.status === statusFilter
+  )
+
   return (
     <main className="mx-auto w-[480px] pt-12">
       <div className="rounded-12 bg-white p-8 shadow-sm">
@@ -25,7 +38,49 @@ const Index = () => {
         </h1>
 
         <div className="pt-10">
-          <TodoList />
+          <Tabs
+            defaultValue="all"
+            onValueChange={(value) => setStatusFilter(value)}
+          >
+            <TabsList className="flex justify-start space-x-2 pb-10">
+              <TabsTrigger
+                value="all"
+                className="h-[44px] w-[66px] rounded-full border-[1px] border-gray-200 data-[state=active]:bg-gray-700 data-[state=active]:text-white"
+              >
+                All
+              </TabsTrigger>
+              <TabsTrigger
+                value="pending"
+                className="h-[44px] w-[104px] rounded-full border-[1px] border-gray-200 data-[state=active]:bg-gray-700 data-[state=active]:text-white"
+              >
+                Pending
+              </TabsTrigger>
+              <TabsTrigger
+                value="completed"
+                className="h-[44px] w-[124px] rounded-full border-[1px] border-gray-200 data-[state=active]:bg-gray-700 data-[state=active]:text-white"
+              >
+                Completed
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="all">
+              <TodoList todos={filteredTodos} />
+            </TabsContent>
+            <TabsContent value="pending">
+              <TodoList
+                todos={filteredTodos.filter(
+                  (todo) => todo.status === 'pending'
+                )}
+              />
+            </TabsContent>
+            <TabsContent value="completed">
+              <TodoList
+                todos={filteredTodos.filter(
+                  (todo) => todo.status === 'completed'
+                )}
+              />
+            </TabsContent>
+          </Tabs>
         </div>
 
         <div className="pt-10">
